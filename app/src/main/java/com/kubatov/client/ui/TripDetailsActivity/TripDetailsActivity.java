df3acf0f8096ccf6948611b24b30d449484d8714
https://github.com/Azim87 <you@example.com>
2019-11-24 21:56:04 +0600

    changed splash icon

--- app/src/main/java/com/kubatov/client/ui/TripDetailsActivity/TripDetailsActivity.java
+++ app/src/main/java/com/kubatov/client/ui/TripDetailsActivity/TripDetailsActivity.java
@@ -3,16 +3,21 @@ package com.kubatov.client.ui.TripDetailsActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.kubatov.client.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class TripDetailsActivity extends AppCompatActivity {
+    private final static String TRIP_DATE = "date";
+    private final static String TRIP_TO = "to";
+    private final static String TRIP_FROM = "from";
+    private final static String TRIP_PRICE = "price";
+    private final static String TRIP_SEATS = "seats";
     @BindView(R.id.trip_text)
     TextView textView;
 
@@ -24,11 +29,11 @@ public class TripDetailsActivity extends AppCompatActivity {
             String tripPrice,
             String tripSeats) {
         Intent intent = new Intent(context, TripDetailsActivity.class);
-        intent.putExtra("date", tripDate);
-        intent.putExtra("to", tripTo);
-        intent.putExtra("from", tripFrom);
-        intent.putExtra("price", tripPrice);
-        intent.putExtra("seats", tripSeats);
+        intent.putExtra(TRIP_DATE, tripDate);
+        intent.putExtra(TRIP_TO, tripTo);
+        intent.putExtra(TRIP_FROM, tripFrom);
+        intent.putExtra(TRIP_PRICE, tripPrice);
+        intent.putExtra(TRIP_SEATS, tripSeats);
         context.startActivity(intent);
     }
 
@@ -37,12 +42,14 @@ public class TripDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_details);
         ButterKnife.bind(this);
+        showDetailedTripInfo();
+    }
+    private void showDetailedTripInfo(){
         String tripDate = getIntent().getStringExtra("date");
         String tripTo = getIntent().getStringExtra("to");
         String tripFrom = getIntent().getStringExtra("from");
         String tripPrice = getIntent().getStringExtra("price");
         String tripSeats = getIntent().getStringExtra("seats");
-        textView.setText("Откуда: -> " + tripFrom + " Куда -> "  + tripTo);
-
+        textView.setText("Откуда: -> " + tripFrom + " Куда -> "  + tripTo + " " + tripSeats);
     }
 }
